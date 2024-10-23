424a8eb6b9cef5c0cb480b14c6f2bd2ca9eea452
Thomas <thomascode92@hotmail.com>
2024-10-23 21:54:51 +0200

    Styling for Navigation & Logo

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -4,12 +4,12 @@ import CrownLogo from "../assets/crown.svg";
 
 export default function NavigationBar() {
   return (
-    <nav>
-      <Link to="/">
+    <nav className="mx-6 mb-6 flex h-16 items-center justify-between">
+      <Link className="flex flex-col justify-around" to="/">
         <CrownLogo data-testid="crown-logo" />
       </Link>
       <ul>
-        <li>
+        <li className="text-lg uppercase">
           <Link to="/shop">shop</Link>
         </li>
       </ul>
