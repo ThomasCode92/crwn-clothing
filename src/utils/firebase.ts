db3956be08af119e0916971434ea35d0fac499a7
Thomas <thomascode92@hotmail.com>
2024-10-28 19:42:25 +0100

    Connect to the Auth Emulator

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,6 +1,13 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  connectAuthEmulator,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 
 import firebaseApp from "../config/firebase";
+
+const FIREBASE_AUTH_EMULATOR = import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_HOST;
 
 const provider = new GoogleAuthProvider();
 
@@ -9,4 +16,10 @@ provider.setCustomParameters({
 });
 
 export const auth = getAuth(firebaseApp);
+
+if (FIREBASE_AUTH_EMULATOR) {
+  console.warn(`Using Firebase Auth Emulator on ${FIREBASE_AUTH_EMULATOR}...`);
+  connectAuthEmulator(auth, "http://" + FIREBASE_AUTH_EMULATOR);
+}
+
 export const signInWithGoogle = () => signInWithPopup(auth, provider);
