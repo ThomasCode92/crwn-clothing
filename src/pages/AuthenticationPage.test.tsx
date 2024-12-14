1d6f608c807ec1777001e14a07b91696e86ef715
Thomas <108367944+ThomasCode92@users.noreply.github.com>
2024-12-14 13:59:36 +0100

    Add GitHub Actions Workflow

--- src/pages/AuthenticationPage.test.tsx
+++ src/pages/AuthenticationPage.test.tsx
@@ -2,6 +2,9 @@ import { render, screen } from "@testing-library/react";
 
 import AuthenticationPage from "./AuthenticationPage";
 
+vi.mock("firebase/auth");
+vi.mock("@/utils/firebase");
+
 test("should render the sign in form", function () {
   render(<AuthenticationPage />);
   expect(screen.getByRole("heading", { name: /sign in/i })).toBeInTheDocument();
