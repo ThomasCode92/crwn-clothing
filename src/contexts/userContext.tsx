835b550b81b2463c1e367e2de2a6ce414e4f2cd1
Thomas <thomascode92@hotmail.com>
2025-01-17 17:05:53 +0100

    Observer: onAuthStateChange

--- src/contexts/userContext.tsx
+++ src/contexts/userContext.tsx
@@ -1,5 +1,16 @@
 import { User } from "firebase/auth";
-import { createContext, Dispatch, SetStateAction, useState } from "react";
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
+
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+} from "@/utils/firebase";
 
 interface IUserContext {
   currentUser: User | null;
@@ -19,6 +30,16 @@ export default function UserContextProvider({
 }) {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
   const value = { currentUser, setCurrentUser };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener(user => {
+      console.log(user);
+      if (user) createUserDocumentFromAuth(user);
+      setCurrentUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 }
