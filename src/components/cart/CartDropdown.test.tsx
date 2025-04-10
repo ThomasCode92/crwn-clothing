0782e416fca92496013798224c0a13597153eb74
Thomas <thomascode92@hotmail.com>
2025-04-10 08:15:41 +0200

    Show Cart Items in Dropdown

--- src/components/cart/CartDropdown.test.tsx
+++ src/components/cart/CartDropdown.test.tsx
@@ -1,10 +1,36 @@
 import { render, screen } from "@testing-library/react";
 
-import CartDropdown from "./CartDropdown";
+import CartDropdown from "@/components/cart/CartDropdown";
+import * as CartItem from "@/components/cart/CartItem";
+import { CartContext, ICartContext } from "@/contexts/cartContext";
+
+function createCartItem(_: unknown, id: number) {
+  return { id, name: `Item ${id + 1}` };
+}
+
+// create a random number of cart items
+const randomNumber = Math.floor(Math.random() * 5) + 1;
+const cartItems = Array.from({ length: randomNumber }, createCartItem);
+
+const cartItem = vi.spyOn(CartItem, "default");
 
 function setup() {
-  render(<CartDropdown />);
+  const ctxValue = { cartItems } as ICartContext;
+  render(
+    <CartContext.Provider value={ctxValue}>
+      <CartDropdown />
+    </CartContext.Provider>,
+  );
 }
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+test("should display a cart item for each item in the cart", function () {
+  setup();
+  expect(cartItem).toHaveBeenCalledTimes(cartItems.length);
+});
 
 test("should render a checkout button", function () {
   setup();
