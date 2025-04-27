d8fb098427bb9f35831e7b4d99ddefeb095d1cdb
Thomas <thomascode92@hotmail.com>
2025-04-27 15:54:30 +0200

    Displaying Checkout Items

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -3,13 +3,26 @@ import { Fragment, useContext, useEffect } from "react";
 import { CartContext } from "@/contexts/cartContext";
 
 export default function CheckoutPage() {
-  const { setIsOpen } = useContext(CartContext);
+  const { cartItems, setIsOpen } = useContext(CartContext);
 
   useEffect(() => setIsOpen(false), [setIsOpen]);
 
   return (
     <Fragment>
       <h1 className="text-2xl font-bold">Checkout</h1>
+      <ul>
+        {cartItems.map(cartItem => {
+          const { id, name, quantity } = cartItem;
+          return (
+            <li key={id}>
+              <h3>{name}</h3>
+              <span>{quantity}</span>
+              <span>decrement</span>
+              <span>increment</span>
+            </li>
+          );
+        })}
+      </ul>
     </Fragment>
   );
 }
