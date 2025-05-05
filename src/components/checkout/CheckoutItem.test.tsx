f7cf1f4aa74021f6ba65681be40ad89b7f237201
Thomas <thomascode92@hotmail.com>
2025-05-05 08:32:45 +0200

    Increment and Decrement Cart Item

--- src/components/checkout/CheckoutItem.test.tsx
+++ src/components/checkout/CheckoutItem.test.tsx
@@ -3,6 +3,8 @@ import { render, screen, within } from "@testing-library/react";
 import { ICartItem } from "@/models/CartItem";
 
 import CheckoutItem from "./CheckoutItem";
+import { CartContext, ICartContext } from "@/contexts/cartContext";
+import userEvent from "@testing-library/user-event";
 
 const cartItem: ICartItem = {
   id: 123,
@@ -12,8 +14,18 @@ const cartItem: ICartItem = {
   quantity: 3,
 };
 
+const cartCtxValue = {
+  addItemToCart: vi.fn(),
+  removeItemFromCart: vi.fn(),
+} as unknown as ICartContext;
+
 function setup() {
-  render(<CheckoutItem {...cartItem} />);
+  render(
+    <CartContext.Provider value={cartCtxValue}>
+      <CheckoutItem {...cartItem} />
+    </CartContext.Provider>,
+  );
+  return userEvent.setup();
 }
 
 test("should render the correct checkout information", function () {
@@ -26,8 +38,28 @@ test("should render the correct checkout information", function () {
   const checkoutItemQuantity = within(checkoutItem).getByText(
     cartItem.quantity,
   );
+  const checkoutItemPrice = within(checkoutItem).getByText(
+    `$${cartItem.price}`,
+  );
   expect(checkoutItemImage).toBeInTheDocument();
   expect(checkoutItemImage).toHaveAttribute("src", cartItem.imageUrl);
   expect(checkoutItemName).toBeInTheDocument();
   expect(checkoutItemQuantity).toBeInTheDocument();
+  expect(checkoutItemPrice).toBeInTheDocument();
+});
+
+test("should decrement the quantity when the decrement button is clicked", async function () {
+  const { click } = setup();
+  const decrementButton = screen.getByRole("button", { name: "❮" });
+  await click(decrementButton);
+  expect(cartCtxValue.removeItemFromCart).toHaveBeenCalledOnce();
+  expect(cartCtxValue.removeItemFromCart).toHaveBeenCalledWith(cartItem);
+});
+
+test("should increment the quantity when the increment button is clicked", async function () {
+  const { click } = setup();
+  const incrementButton = screen.getByRole("button", { name: "❯" });
+  await click(incrementButton);
+  expect(cartCtxValue.addItemToCart).toHaveBeenCalledOnce();
+  expect(cartCtxValue.addItemToCart).toHaveBeenCalledWith(cartItem);
 });
--- src/components/checkout/CheckoutItem.tsx
+++ src/components/checkout/CheckoutItem.tsx
@@ -1,22 +1,43 @@
+import { useContext } from "react";
+
+import { CartContext } from "@/contexts/cartContext";
 import { ICartItem } from "@/models/CartItem";
 
 export interface CheckoutItemProps extends ICartItem {}
 
 export default function CheckoutItem({
+  id,
   name,
   quantity,
   imageUrl,
+  price,
 }: CheckoutItemProps) {
+  const { addItemToCart, removeItemFromCart } = useContext(CartContext);
+
+  const cartItem: ICartItem = { id, name, quantity, imageUrl, price };
+
+  function incrementItemQuantity() {
+    addItemToCart(cartItem);
+  }
+
+  function decrementItemQuantity() {
+    removeItemFromCart(cartItem);
+  }
+
   return (
     <li className="*:px-1">
       <img src={imageUrl} alt={name} />
       <h3>{name}</h3>
       <span className="flex items-center gap-2">
-        <button className="cursor-pointer">&#10094;</button>
+        <button className="cursor-pointer" onClick={decrementItemQuantity}>
+          &#10094;
+        </button>
         {quantity}
-        <button className="cursor-pointer">&#10095;</button>
+        <button className="cursor-pointer" onClick={incrementItemQuantity}>
+          &#10095;
+        </button>
       </span>
-      <span>{`$${quantity}`}</span>
+      <span>{`$${price}`}</span>
       <button>&#10005;</button>
     </li>
   );
