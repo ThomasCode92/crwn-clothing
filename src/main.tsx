04adad98f16d7a1c5b18dc359c281c9f07d7b813
Thomas <thomascode92@hotmail.com>
2025-05-15 08:26:32 +0200

    Setting Up the Store

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
+import CartContextProvider from "@/contexts/cartContext.tsx";
 import CategoriesContextProvider from "@/contexts/categoriesContext.tsx";
 import UserContextProvider from "@/contexts/userContext.tsx";
-import CartContextProvider from "./contexts/cartContext.tsx";
+import { store } from "@/store/store.ts";
 
 import App from "./App.tsx";
 import "./index.css";
@@ -11,13 +13,15 @@ import "./index.css";
 const rootEl = document.getElementById("root");
 
 createRoot(rootEl!).render(
-  <StrictMode>
-    <UserContextProvider>
-      <CategoriesContextProvider>
-        <CartContextProvider>
-          <App />
-        </CartContextProvider>
-      </CategoriesContextProvider>
-    </UserContextProvider>
-  </StrictMode>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <UserContextProvider>
+        <CategoriesContextProvider>
+          <CartContextProvider>
+            <App />
+          </CartContextProvider>
+        </CategoriesContextProvider>
+      </UserContextProvider>
+    </Provider>
+  </React.StrictMode>,
 );
---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import {
+  applyMiddleware,
+  compose,
+  legacy_createStore as createStore,
+} from "redux";
+import logger from "redux-logger";
+
+import { rootReducer } from "@/store/root-reducer";
+
+const middlewares = [logger];
+const composedEnhancers = compose(applyMiddleware(...middlewares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
